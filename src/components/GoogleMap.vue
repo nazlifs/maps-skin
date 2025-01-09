9e2b80aab116ac3a36850b98af4244ff14a5ae15
nazlifs <nazlifadlan@gmail.com>
2025-01-09 12:15:58 +0700

    mengubah skin/style maps

--- src/components/GoogleMap.vue
+++ src/components/GoogleMap.vue
@@ -6,12 +6,12 @@
 
 <script>
 export default {
-  name: 'GoogleMap',
+  name: "GoogleMap",
   mounted() {
     if (window.google) {
       this.initMap();
     } else {
-      const script = document.createElement('script');
+      const script = document.createElement("script");
       script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAP_API_KEY}&callback=initMap`;
       script.async = true;
       script.defer = true;
@@ -22,42 +22,74 @@ export default {
   },
   methods: {
     initMap() {
+      const customStyles = [
+        {
+          elementType: "geometry",
+          stylers: [{ color: "#ebe3cd" }],
+        },
+        {
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#523735" }],
+        },
+        {
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#f5f1e6" }],
+        },
+        {
+          featureType: "administrative",
+          elementType: "geometry.stroke",
+          stylers: [{ color: "#c9b2a6" }],
+        },
+        {
+          featureType: "landscape",
+          elementType: "geometry",
+          stylers: [{ color: "#dfd2ae" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#f5f1e6" }],
+        },
+      ];
+
       if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
           (position) => {
             const { latitude, longitude } = position.coords;
 
-            const map = new google.maps.Map(document.getElementById('map'), {
-              center: { lat: latitude, lng: longitude }, 
+            const map = new google.maps.Map(document.getElementById("map"), {
+              center: { lat: latitude, lng: longitude },
               zoom: 14,
+              styles: customStyles,
             });
 
             new google.maps.Marker({
               position: { lat: latitude, lng: longitude },
               map: map,
-              title: 'Your Location',
+              title: "Your Location",
             });
           },
           () => {
             alert("Gagal mendapatkan lokasi Anda.");
-            this.loadDefaultMap();
+            this.loadDefaultMap(customStyles);
           }
         );
       } else {
         alert("Geolocation tidak didukung oleh browser Anda.");
-        this.loadDefaultMap();
+        this.loadDefaultMap(customStyles);
       }
     },
-    loadDefaultMap() {
-      const map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -6.200000, lng: 106.816666 }, 
+    loadDefaultMap(styles) {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -6.200000, lng: 106.816666 },
         zoom: 10,
+        styles: styles,
       });
 
       new google.maps.Marker({
         position: { lat: -6.200000, lng: 106.816666 },
         map: map,
-        title: 'Jakarta',
+        title: "Jakarta",
       });
     },
   },
