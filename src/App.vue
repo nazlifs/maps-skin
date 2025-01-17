30259153749c1da8727cf337f4b194f2afd37364
nazlifs <nazlifadlan@gmail.com>
2025-01-17 17:31:06 +0700

    ... map nya masih belum move sesuai lokasi yang dipilih

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,6 @@ export default {
   methods: {
     moveToLocationHandler(lat, lng) {
       console.log(`Navigating to latitude: ${lat}, longitude: ${lng}`);
-      // Implementasikan logika navigasi di sini
     },
   },
 };
